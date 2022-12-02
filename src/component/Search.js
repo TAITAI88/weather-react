ebf8e1cdc396dc5d28f1cd48c0dd0b6e69e3fcc2
TAITAI88 <z3607551@gmail.com>
2022-12-02 11:30:02 +0800

    deploy website

--- src/component/Search.js
+++ src/component/Search.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 const Search = () => {
   let [data, setData] = useState(null);
   const auth = "563492ad6f917000010000014f56f993b2254f0dbddc7aaf09c5ce9c";
-  const intialURL = "https://www.travel.taipei/open-api/zh-tw/Attractions/All";
+  const intialURL =
+    "https://travel.tycg.gov.tw/open-api/zh-tw/Travel/Attraction?regions=%E4%B8%8D%E9%99%90&category=%E4%B8%8D%E9%99%90&page=1";
   const search = async () => {
     const dataFetch = await fetch(intialURL, {
       method: "GET",
