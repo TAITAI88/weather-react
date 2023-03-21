9e5623d7925b02b355ac8df7e3baf8827b670ac1
TAITAI88 <z3607551@gmail.com>
2023-03-21 22:46:15 +0800

    Modify web name and fix e some mistake.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-practice">
+    <BrowserRouter basename="/weather-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
